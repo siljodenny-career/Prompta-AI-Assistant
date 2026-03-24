e817a3f80879352560c9a173de9b17dd1ad9198e
Siljo Denny <siljodennyjohn>
2026-03-24 11:24:17 +0530

    device preview added

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -5,6 +5,7 @@ import 'package:client/features/chat/domain/usecases/send_chat_usecase.dart';
 import 'package:client/features/chat/presentation/blocs/chat_bloc/chat_bloc.dart';
 import 'package:client/features/chat/presentation/pages/onboarding_page.dart';
 import 'package:client/core/theme/app_theme.dart';
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -16,7 +17,9 @@ void main() {
   runApp(
     BlocProvider(
       create: (_) => ChatBloc(sendChatUsecase: useCase),
-      child: const MyApp(),
+      child: DevicePreview(
+        builder: (context) => const MyApp(),
+      ),
     ),
   );
 }
